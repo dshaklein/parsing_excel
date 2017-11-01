34c82217ab17579a5c8b55bcbe9cabffb3b6af00
dshaklein <d.shaklein@sailplay.ru>
2017-11-01 20:21:38 +0300

    fixed phone

--- main.py
+++ main.py
@@ -4,43 +4,48 @@ from openpyxl import load_workbook, Workbook
 from collections import defaultdict
 
 rows_to_pass = set()
+card_numbers = []
 
 
 def fix_card_numbers(sheet, key):
     for i, col in enumerate(sheet[key],start=1):
         if i == 1: continue
         num = str(col.value)
+        card_numbers.append(num)
         if not re.match('^\d+$', num):
             print(i, num)
             rows_to_pass.add(i)
+
+
+def make_phone(phone, i):
+    if phone is None:
+        return 'NULL'
+    phone = re.sub('[^\d]', '', str(phone))
+    if phone == '' or phone == 'NULL':
+        return 'NULL'
+    if re.match('^[^78]\d{9}$', phone):
+        phone = '7{}'.format(phone)
+        print('row: {} 10 digit number + 7: {}'.format(i, phone))
+    if re.match('^8\d{10}$', phone):
+        phone = '7' + phone[1:]
+        print('row: {} replace 8 with 7: {}'.format(i, phone))
+    if len(phone) != 11:
+        return 'NULL'
+    return phone
 
 
 def fix_phones(sheet, key1, key2):
     phones = defaultdict(int)
     for i, (col, col2) in enumerate(zip(sheet[key1], sheet[key2]), start=1):
         if i == 1 or i in rows_to_pass: continue
-        phone = str(col.value)
-        phone = re.sub('[^\d]', '', phone)
-
-        if phone == 'None' or phone == '':
-            phone2 = str(col2.value)
-            phone2 = re.sub('[^\d]', '', phone2)
-            if phone2 != 'None' and phone2 != '':
+        phone = make_phone(col.value, i)
+        if phone == 'NULL':
+            phone2 = make_phone(col2.value, i)
+            if phone2 != 'NULL':
                 phone = phone2
             else:
                 col.value = 'NULL'
                 continue
-        if re.match('^[^78]\d{9}$', phone):
-            phone = '7{}'.format(phone)
-            print('10 digit number + 7: {}'.format(phone))
-        if re.match('^8\d{10}$', phone):
-            print(phone)
-            phone = '7' + phone[1:]
-            print('replace 8 with 7: {}'.format(phone))
-        if len(phone) != 11:
-            print('not 11 digit number: {}'.format(phone))
-            col.value = 'NULL'
-            continue
         phones[phone] += 1
         col.value = int(phone)
     return phones
@@ -70,11 +75,11 @@ def find_equal(sheet, key, items):
     for i, col in enumerate(sheet[key], start=1):
         if i == 1 or i in rows_to_pass: continue
         item = str(col.value)
-        if item in repeated_items.keys():
+        if item in repeated_items:
             repeated_rows[item].append(i)
     for k in repeated_rows:
         print(k)
-        repeated_rows[k].sort(key=lambda index: sheet[index][0].value)
+        repeated_rows[k].sort(key=lambda index: card_numbers[index])
     return repeated_rows
 
 
@@ -110,14 +115,17 @@ if __name__ == '__main__':
 
     emails = fix_emails(original['cols-2-upload'], 'D')
     print('Fix emails finished')
+    print('Find equal phones')
     phone_1_cells = find_equal(original['cols-2-upload'], 'E', phones_1)
+    print('Find equal emails')
     email_cells = find_equal(original['cols-2-upload'], 'D', emails)
-
+    print('Merge phones')
     merge_data(original['cols-2-upload'], phone_1_cells)
+    print('Merge emails')
     merge_data(original['cols-2-upload'], email_cells)
 
     fix_card_numbers(original['cols-2-upload'], 'A')
-
+    print('Start saving')
     new_wb = Workbook()
     ws = new_wb.active
     ws.title = 'cols-2-upload'
